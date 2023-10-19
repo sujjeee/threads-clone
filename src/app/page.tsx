100135f9d8fe6372c7fdeb99747018845970a8ed
Suraj <surajgupta.college@gmail.com>
2023-10-19 11:25:10 +0530

    added shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import { CreatePost } from "@/app/_components/create-post";
+import { CreatePost } from "@/components/create-post";
 import { api } from "@/trpc/server";
 
 export default async function Home() {
