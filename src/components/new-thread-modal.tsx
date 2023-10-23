c6fd762aa2b959484ca314b595fe6beed8a76030
Suraj <surajgupta.college@gmail.com>
2023-10-23 11:53:40 +0530

    add react dropzone

--- src/components/new-thread-modal.tsx
+++ src/components/new-thread-modal.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react'
+import React, { useCallback, useLayoutEffect, useRef, useState } from 'react'
 import { Card } from './ui/card'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { Image } from 'lucide-react'
@@ -14,9 +14,106 @@ import {
 import { Icons } from './icons'
 import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
+import {
+    useDropzone,
+    type Accept,
+    type FileRejection,
+} from "react-dropzone";
+import { toast } from 'sonner'
 
 export default function NewThreadModal({ showIcon }: { showIcon: boolean }) {
+
+    const [files, setFiles] = React.useState<File | undefined>(undefined)
+
+    const [FileData, setFileData] = React.useState()
+
+    const [previewURL, setPreviewURL] = React.useState<string | undefined>(undefined)
+
     const path = usePathname()
+    function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+        if (textArea == null) return;
+        textArea.style.height = "0";
+        textArea.style.height = `${textArea.scrollHeight}px`;
+    }
+
+    const [inputValue, setInputValue] = useState("");
+    const textAreaRef = useRef<HTMLTextAreaElement>();
+    const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+        updateTextAreaSize(textArea);
+        textAreaRef.current = textArea;
+    }, []);
+
+    useLayoutEffect(() => {
+        updateTextAreaSize(textAreaRef.current);
+    }, [inputValue]);
+
+
+    const maxSize = 10 * 1024 * 1024;
+
+    const onDrop = React.useCallback(
+        (acceptedFiles: File[], rejectedFiles: FileRejection[]) => {
+            const acceptedFile = acceptedFiles[0];
+            if (!acceptedFile) {
+                alert('Please select a single file to upload.');
+                return;
+            }
+            // @ts-ignore
+            setFileData(acceptedFile)
+
+            const fileWithPreview = Object.assign(acceptedFile, {
+                preview: URL.createObjectURL(acceptedFile),
+            });
+            setFiles(fileWithPreview);
+
+            const previewURL = URL.createObjectURL(acceptedFile)
+            setPreviewURL(previewURL)
+
+            if (rejectedFiles.length > 0) {
+                rejectedFiles.forEach(({ errors }) => {
+                    if (errors[0]?.code === "file-too-large") {
+                        toast.error(`File is too large. Max size is 1 MB`);
+                        return;
+                    }
+                    errors[0]?.message && toast.error(errors[0].message);
+                });
+            }
+        },
+        [maxSize, setFiles]
+    );
+    const accept: Accept = {
+        "image/*": [],
+    }
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        accept,
+        maxSize,
+    })
+
+    const highlightMentions = (e: any) => {
+        const inputValue = e.target.value;
+
+        const formattedText = inputValue.split(/(@\w+|@\w+)/).map((part: any, index: any) => {
+            if (part.startsWith('@')) {
+                return <span key={index} className="font-semibold text-blue-500">{part}</span>;
+            } else {
+                return <span key={index}>{part}</span>;
+            }
+        });
+
+        console.log("formattedText", formattedText)
+        // // Regular expression to find mentions starting with @
+        // const mentionRegex = /@(\w+)/g;
+
+        // // Replace mentions with a span element for styling
+        // const highlightedText = inputValue.replace(
+        //     mentionRegex,
+        //     '<span class="text-blue-800 font-bold">@$1</span>'
+        // );
+
+        // Update the state with the highlighted text
+        setInputValue(formattedText);
+    };
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -55,19 +152,30 @@ export default function NewThreadModal({ showIcon }: { showIcon: boolean }) {
                 <Card className=" w-full p-0 rounded-2xl border bg-[#181818] ">
                     <div className='overflow-y-auto no-scrollbar p-6  pb-4 max-h-[70vh] '>
                         <div className="flex space-x-3">
-                            <Avatar className='h-8 w-8'>
+                            <Avatar className='h-9 w-9'>
                                 <AvatarImage src="https://avatar.vercel.sh/5" />
                                 <AvatarFallback>JL</AvatarFallback>
                             </Avatar>
-                            <div className='flex flex-col gap-1.5'>
+                            <div className='flex flex-col gap-1.5 w-full'>
                                 <p className="text-sm font-medium leading-none">Jackson Lee</p>
-
-                                <p className="text-sm text-accent-foreground break-words pr-4 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio minus, voluptatibus cupiditate magni quo ab consequatur, ex laborum, unde sint saepe quas? Delectus animi tempore aliquid nobis esse. Magni saepe vel impedit quidem, sed iste?
-                                </p>
-                                <div className='my-3  overflow-hidden rounded-xl w-fit'>
-                                    <img src="https://images.pexels.com/photos/4048598/pexels-photo-4048598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='object-contain max-h-[500px] max-w-full border' />
+                                <textarea
+                                    ref={inputRef}
+                                    style={{ height: 0 }}
+                                    value={inputValue}
+                                    onChange={highlightMentions}
+                                    className="flex-grow resize-none overflow-hidden outline-none text-[15px] text-accent-foreground break-words bg- w-full bg-transparent"
+                                    placeholder="What's happening?"
+                                />
+                                {files && (
+                                    <div className='my-3 overflow-hidden rounded-xl w-fit'>
+                                        <img src={previewURL} alt="" className='object-contain max-h-[500px] max-w-full border' />
+                                    </div>
+                                )}
+                                <div {...getRootProps()}>
+                                    <input {...getInputProps()} />
+                                    <Icons.image className='h-5 w-5 mt-2 text-[#777777]  transform active:scale-75 transition-transform cursor-pointer' />
                                 </div>
-                                <Image className='h-5 w-5 mt-2 text-muted' />
+
                             </div>
                         </div>
                     </div>
